06a51fc6ea48eb69a7fe0264bf2f9d1791b853a6
rishi monohur <rishi.monohur15@gmail.com>
2023-08-10 14:08:25 +0400

    3 seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "Cleaning database..."
 Flat.destroy_all
-
-puts "Creating flats..."
 
 5.times do
   flat = Flat.new(
